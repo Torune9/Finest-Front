88a38573576c55071038af26fc7b3b80a5270c6a
rahman <rahmannurhayadi@gmail.com>
2024-02-09 17:08:42 +0700

    feat : add middleware

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import pinia from './service/pinia'
+import router from './router/routers'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import library from './service/fontAwesome'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(library)
+app.use(pinia)
+app.use(router)
+app.component('font-awesome-icon',FontAwesomeIcon)
+app.mount('#app')
