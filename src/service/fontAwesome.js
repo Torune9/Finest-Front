583b85c87a41b05c7d1b93735a3073e54bb80e22
rahman <rahmannurhayadi@gmail.com>
2024-02-09 17:09:29 +0700

    feat : add new service

---
+++ src/service/fontAwesome.js
@@ -0,0 +1,72 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { 
+    faEye,
+    faEyeSlash,
+    faLock,
+    faUser,
+    faEnvelope,
+    faRectangleXmark,
+    faGear,
+    faImages,
+    faCircleXmark,
+    faPenToSquare,
+    faTrashCan,
+    faMagnifyingGlass,
+    faSquarePlus,
+    faTrashCanArrowUp,
+    faSpinner,
+    faHouse,
+    faSuitcase,
+    faUsers,
+    faShoePrints,
+    faTableList,
+    faWindowMaximize,
+    faFileCircleQuestion,
+    faBars,
+    faCamera,
+    faFloppyDisk,
+    faShrimp,
+    faCircleNotch,
+    faMinus,
+    faPlus
+} from '@fortawesome/free-solid-svg-icons'
+import { faSquareCheck} from '@fortawesome/free-regular-svg-icons'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+
+export default {
+    install(){
+        library.add(
+            faEye,
+            faEyeSlash,
+            faSquareCheck,
+            faUser,
+            faLock,
+            faEnvelope,
+            faRectangleXmark,
+            faGear,
+            faImages,
+            faCircleXmark,
+            faPenToSquare,
+            faTrashCan,
+            faMagnifyingGlass,
+            faSquarePlus,
+            faTrashCanArrowUp,
+            faSpinner,
+            faHouse,
+            faSuitcase,
+            faUsers,
+            faShoePrints,
+            faTableList,
+            faWindowMaximize,
+            faFileCircleQuestion,
+            faBars,
+            faCamera,
+            faFloppyDisk,
+            faShrimp,
+            faCircleNotch,
+            faMinus,
+            faPlus,
+            faGoogle
+            )
+    }
+}
